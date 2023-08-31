58f0906423ff698535afb46ae038f63ff50dbda1
Andre Pinto <andre.marpinto@gmail.com>
2023-08-31 12:36:49 +0100

    login and signup functions

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import { useContext, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Context/auth.context";
+
+const API_URL = 'http://localhost:5005';
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const {storeToken, authenticateUser} = useContext(AuthContext)
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      
+      const requestBody = {email, password}
+
+      axios.post(`${API_URL}/auth/login`, requestBody)
+      .then((response) => {
+          storeToken(response.data.authToken);
+/*             debugger; */
+          authenticateUser();
+/*             debugger; */
+          navigate('/')
+      })
+      .catch((error)=>{
+          const errorDescription = error.response.data.message;
+          setErrorMessage(errorDescription);
+      })
+  }
   return (
     <div>
 
--- src/Pages/Signup/index.jsx
+++ src/Pages/Signup/index.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = 'http://localhost:5005';
 
 function SignupPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const requestBody = {username, firstName, lastName, email, password};
+
+      axios.post(`${API_URL}/auth/signup`, requestBody)
+      .then((response)=>{
+          navigate('/login')
+      })
+      .catch((error) =>{
+          const errorDescription = error.response.data.message;
+          setErrorMessage(errorDescription);
+      })
+  }
+
   return (
     <div>
 
