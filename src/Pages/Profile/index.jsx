4b85b841655e8367d57a74f7557936bec565f241
Manuel Segarra <manuel.gs.segarra@gmail.com>
2023-09-07 19:38:34 +0100

    users can now send messages from friend's profile

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -7,6 +7,10 @@ import { AuthContext } from "../../Context/auth.context";
 import * as React from "react";
 import Button from "@mui/material/Button";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TextField from "@mui/material/TextField";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
 
 const API_URL = "http://localhost:5005";
 
@@ -21,6 +25,8 @@ function ProfilePage() {
   const [friendRequestRemoved, setFriendRequestRemoved] = useState(false);
   const [loading, setLoading] = useState(true);
   const [isFriend, setIsFriend] = useState(false);
+  const [hiddenMessage, setHiddenMessage] = useState(true);
+  const [content, setContent] = useState("");
 
   // Function to add friend button
   const addFriend = () => {
@@ -33,6 +39,25 @@ function ProfilePage() {
     setFriendRequestRemoved(true);
     setFriendAdded(false);
     setIsFriendAdded(false);
+  };
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    const requestBody = { content };
+
+    axios
+      .post(`${API_URL}/api/message/${profileUser._id}/create`, requestBody, {
+        headers: {
+          Authorization: `Bearer ${storedToken}`,
+        },
+      })
+      .then(() => {
+        console.log("Message sent successfully");
+        setContent("");
+        setHiddenMessage(true);
+      })
+      .catch((error) => console.log(error));
   };
 
   // Get profile user information
@@ -50,13 +75,14 @@ function ProfilePage() {
         if (response.data.profileUser.friendRequests.includes(user.user._id)) {
           setIsFriendAdded(true);
         }
-        if(response.data.profileUser.friends){
-        response.data.profileUser.friends.map((friend)=>{
-          if (friend._id === user.user._id){
-            setIsFriend(true);
-          }
-        })}
-        
+        if (response.data.profileUser.friends) {
+          response.data.profileUser.friends.map((friend) => {
+            if (friend._id === user.user._id) {
+              setIsFriend(true);
+            }
+          });
+        }
+
         setLoading(false);
       })
       .catch((error) => console.log(error));
@@ -109,40 +135,94 @@ function ProfilePage() {
     }
   }, [friendRequestRemoved]);
 
-  console.log(isFriend)
   return (
     <div style={{ paddingTop: "72px" }}>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div>{!isFriend ? (
-          !isFriendAdded ? (
-            <Button
-              variant="contained"
-              color="error"
-              startIcon={<PersonAddIcon />}
-              onClick={addFriend}
-            >
-              Add Friend
-            </Button>
+        <div>
+          {!isFriend ? (
+            !isFriendAdded ? (
+              <Button
+                variant="contained"
+                color="error"
+                startIcon={<PersonAddIcon />}
+                onClick={addFriend}
+              >
+                Add Friend
+              </Button>
+            ) : (
+              <Button
+                variant="outlined"
+                color="error"
+                startIcon={<PersonAddIcon />}
+                onClick={removeFriendRequest}
+              >
+                Remove friend request
+              </Button>
+            )
           ) : (
-            <Button
-              variant="outlined"
-              color="error"
-              startIcon={<PersonAddIcon />}
-              onClick={removeFriendRequest}
-            >
-              Remove friend request
-            </Button>
-          )) : (
-            <Button
-              variant="outlined"
-              color="error"
-              startIcon={<PersonAddIcon />}
-              onClick={sendMessage}
-            >
-              Send Message
-            </Button>
+            <div>
+              <form onSubmit={handleSendMessage}>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  startIcon={<PersonAddIcon />}
+                  onClick={() => setHiddenMessage(false)}
+                >
+                  Send Message
+                </Button>
+                {!hiddenMessage && (
+                  <div>
+                    <TextField
+                      id="outlined-multiline-static"
+                      label=""
+                      color="primary"
+                      multiline
+                      rows={9}
+                      name="content"
+                      value={content}
+                      onChange={(e) => setContent(e.target.value)}
+                      sx={{
+                        bgcolor: "rgba(255,255,255,.6)",
+                        borderRadius: "5px",
+                        width: "500px",
+                      }}
+                    />
+
+                    {
+                      <Stack
+                        direction="row"
+                        spacing={6.6}
+                        sx={{ marginTop: "8px" }}
+                      >
+                        <Button
+                          onClick={() => {
+                            setContent("");
+                            setHiddenMessage(true);
+                          }}
+                          variant="outlined"
+                          sx={{
+                            bgcolor: "rgba(255,255,255,.6)",
+                            borderRadius: "5px",
+                          }}
+                          startIcon={<DeleteIcon />}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          variant="contained"
+                          endIcon={<SendIcon />}
+                          type="submit"
+                        >
+                          Send
+                        </Button>
+                      </Stack>
+                    }
+                  </div>
+                )}
+              </form>
+            </div>
           )}
           <h1>
             Artist {profileUser.firstName} {profileUser.lastName} profile
